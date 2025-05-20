6eb6e41da32d5a00d2d5bf9a890a3d2afc52dc73
palandeta <palandeta@utn.edu.ec>
2025-05-19 20:06:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return "<h1><center>Hola mundo. Att Pablo Landeta</center></h1>"
+    return "<h1><center>Hola mundo. Att Pablo Landeta L</center></h1>"
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
